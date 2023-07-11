a51a3cb1604ee916f2d09048dd33534ea9c0fd4b
shivam0277 <114799765+shivam0277@users.noreply.github.com>
2023-07-12 00:49:12 +0530

    added comments

--- move_elements.java
+++ move_elements.java
@@ -3,14 +3,14 @@ class Solution {
 
         int i = 0;
         for (int num : nums) {
-            if (num != 0) {
-                nums[i] = num;
+            if (num != 0) {                           // If the current number is not zero,
+                nums[i] = num;                        // move it to the front of the array               
                 i++;
             }
         }
 
         while (i < nums.length) {
-            nums[i] = 0;
+            nums[i] = 0;                             // Fill the remaining elements with zeroes
             i++;
         }
 
