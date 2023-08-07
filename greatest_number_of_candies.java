e0a337bbf47c1db6d410645bd8941d74c47d7232
Jose Morales <j_morales@csu.fullerton.edu>
2023-08-07 06:08:02 -0700

    added comments

--- greatest_number_of_candies.java
+++ greatest_number_of_candies.java
@@ -6,15 +6,18 @@ class Solution {
 
         int max = 0;
 
+        // finds max within the given array
         for (int iy = 0; iy < size; iy++) {
             if (candies[iy] > max) {
                 max = candies[iy];
             }
         }
 
+        // this for loops attempts to add extra candies to each ix index
         for (int ix = 0; ix < size; ix++) {
             int num = candies[ix] + extraCandies;
 
+            // if after the candies is added, check if its greater than the previous max
             if (num >= max) {
                 System.out.println("ans-> " + num + "ix >= " + max + " TRUE");
                 result.add(true);
