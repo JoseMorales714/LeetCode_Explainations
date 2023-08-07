4470604426d03159f1ca4b90460ebf199504c262
Jose Morales <j_morales@csu.fullerton.edu>
2023-08-07 06:11:24 -0700

    reverses vowles in string with comments

---
+++ reverse_vowels.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String reverseVowels(String s) {
+
+        StringBuilder str = new StringBuilder();
+
+        // this ensures any and all vowels are added to our experimental string
+        for (char x : s.toCharArray()) {
+            if ((x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' || x == 'A'
+                    || x == 'E' || x == 'I' || x == 'O' || x == 'U')) {
+                str.append(x);
+            }
+        }
+
+        // reverse our string
+        str = str.reverse();
+
+        StringBuilder rtn_str = new StringBuilder();
+        int counter = 0;
+
+        // this for loops will check for any and all vowels again
+        // if true then will append to our string wit current char at the next inde
+        // else will append to our string at current index
+        for (char x : s.toCharArray()) {
+            if ((x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' || x == 'A'
+                    || x == 'E' || x == 'I' || x == 'O' || x == 'U')) {
+                rtn_str.append(str.charAt(counter++));
+            } else {
+                rtn_str.append(x);
+            }
+        }
+
+        return rtn_str.toString();
+
+    }
+}
